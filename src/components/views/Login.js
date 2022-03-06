cc790f5113b918611d3fed132f4bbf7847bc4dc0
Ethan Ohlin <ethan.ohlin@uzh.ch>
2022-03-06 11:52:49 +0100

    Update Login.js

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -21,7 +21,7 @@ const FormField = props => {
       </label>
       <input
         className="login input"
-        placeholder="enter here..."
+        placeholder="enter here...?"
         value={props.value}
         onChange={e => props.onChange(e.target.value)}
       />
@@ -64,7 +64,7 @@ const Login = props => {
       <div className="login container">
         <div className="login form">
           <FormField
-            label="Username"
+            label="User name"
             value={username}
             onChange={un => setUsername(un)}
           />
